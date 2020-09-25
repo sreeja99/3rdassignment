5b8650cdf453398a212dcb23116ecb430c51c89a
sreeja99 <srijagodishala@gmail.com>
2020-09-25 17:42:53 +0530

    Added:Welcome Statement is Added

---
+++ CoordinateGeometry.java
@@ -0,0 +1,11 @@
+package geometry;
+
+public class coorGeo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to the Line Comparision Computation Program on Master Branch");
+
+	}
+
+}

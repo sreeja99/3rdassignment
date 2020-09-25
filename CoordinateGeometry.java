e6dcfe6fe10ab65a3f96e9abde583e3a7762cfa9
sreeja99 <srijagodishala@gmail.com>
2020-09-25 21:31:44 +0530

    Added:Calculating Length Of Line

--- CoordinateGeometry.java
+++ CoordinateGeometry.java
@@ -3,9 +3,14 @@ package geometry;
 public class coorGeo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Welcome to the Line Comparision Computation Program on Master Branch");
-
+        int x1 = (int)(Math.floor(Math.random()*10));
+        int y1 = (int)(Math.floor(Math.random()*10));
+        int x2 = (int)(Math.floor(Math.random()*10));
+        int y2 = (int)(Math.floor(Math.random()*10));
+        int sumOfSqrs = ((x1-x2)*(x1-x2))+((y1-y2)*(y1-y2));
+        System.out.println("Point 1:"+x1+","+y1);
+        System.out.println("Point 2:"+x2+","+y2);
+        System.out.println("Length of the Line:"+Math.sqrt(sumOfSqrs));
 	}
 
 }
